03d5db4b166cbbd68fbd9e1147939a9b5c9a50cd
Louis Rossier-Tillard <l.rossier-tillard@rocroysvp.fr>
2025-10-30 15:55:23 +0100

    Ajout du tour par tour et refonte avec ajout de POO

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from chess import *
 from canvas_tkinter import *
+#import ... as IA1
+#import ... as IA2
+
+"""
+Décommentez les imports pour y mettre votre fichier d'IA
+"""
 
 board = Board()
+root = Tk()
+root.title("Echecs")
 
-board.push_san('Nf3')
+"""
+Rajoutez le nom de votre fichier pour jouer à votre Jeu et en entrée de la fonction classe Chess_UI
+"""
 
-update_board(board)
+c = Chess_UI(root, board, None, None)
 
 root.mainloop()
